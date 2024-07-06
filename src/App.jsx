b7d1851db41e450d1186bddcf1fd261c757dd654
yuimorii <yuimoriistudio@gmail.com>
2024-07-06 17:53:24 -0500

    toilet

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import './App.css'
+import { Canvas } from '@react-three/fiber'
+
+const App = () => {
+
+  return (
+    <Canvas>
+      <directionalLight position={[0,0,2]} />
+      <mesh position={[1,0,0]}>
+        <boxGeometry/>
+        <meshStandardMaterial color={"orange"} />
+      </mesh>
+        <mesh position={[-1,2,0]}>
+        <boxGeometry/>
+        <meshStandardMaterial color={"orange"} />
+      </mesh>
+        <mesh position={[-1,0,0]}>
+        <boxGeometry/>
+        <meshStandardMaterial color={"orange"} />
+      </mesh>
+        <mesh position={[1,2,0]}>
+        <boxGeometry/>
+        <meshStandardMaterial color={"orange"} />
+      </mesh>
+    </Canvas>
+  )
+}
+
+export default App
+
+ 
